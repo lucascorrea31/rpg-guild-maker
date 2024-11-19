fc039fd86502bcc4e2317004dd1ee04f42857ebf
Lucas Correa <lucas.nhl@gmail.com>
2024-11-19 14:14:57 -0300

    add logo em footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,9 @@ const App = () => {
 
 	return (
 		<div className="container mx-auto px-4">
+			<div className="w-fit p-4 absolute left-16 top-2 text-2xl font-black antialiased tracking-tight rounded-full shadow-sm shadow-slate-800/30 bg-slate-100/30 text-black cursor-pointer hover:shadow-md hover:brightness-150 hover:scale-105 transition-all">
+				GM
+			</div>
 			<h1 className="text-3xl font-bold py-4 text-center mb-4">
 				RPG Guild Maker
 			</h1>
@@ -21,6 +24,16 @@ const App = () => {
 				<PlayerList key={refresh} />
 			</div>
 			<GuildList />
+			<div className="w-full mx-auto my-4 py-2 px-4 text-center text-sm italic text-slate-500">
+				Criado por:{" "}
+				<a
+					href="https://linktr.ee/lucascorrea31"
+					target="_blank"
+					className="hover:underline hover:text-black hover:scale-105 transition-all">
+					Lucas Tadeu Correa de Oliveira
+				</a>{" "}
+				@ {new Date().getFullYear()}
+			</div>
 		</div>
 	);
 };
