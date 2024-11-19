7f98a6b208d868be3b24b5012d2365181ed72779
Lucas Correa <lucas.nhl@gmail.com>
2024-11-19 15:42:36 -0300

    update url domain

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost:8000/api",
+	baseURL: "https://rpg-guild-maker-backend-qmcc9njsu.vercel.app/api",
 });
 
 export default api;
